a68e4832abe7a562e0cbf3008e1b9b46d244ce49
Jonas Regehr <jo.sc.re.04@gmail.com>
2024-06-11 16:27:29 -0600

    plugin working

--- main.rkt
+++ main.rkt
@@ -20,15 +20,16 @@
 ;; See the current version of the racket style guide here:
 ;; http://docs.racket-lang.org/style/index.html
 
-(require ffi/unsafe fpbench herbie/plugin racket/match)
+(require ffi/unsafe fpbench racket/match)
 
-(eprintf "Loading vdt accelerators\n")
-
+;(eprintf "Loading vdt accelerators\n")
+(provide vdt-exp  vdt-sin  vdt-cos  vdt-tan  vdt-tanh  vdt-log  vdt-acos  vdt-asin  vdt-atan  vdt-isqrt
+         vdt-expf vdt-sinf vdt-cosf vdt-tanf vdt-tanhf vdt-logf vdt-acosf vdt-asinf vdt-atanf vdt-isqrtf)
 (define vdt-wrapper "vdtwrapper")
 
 (set-c-header!
  (lambda (previous)
-   (string-append (previous) "#include <vdtwrapper.h>\n")))
+   (string-append (previous) "#include <vdt-wrapper.h>\n")))
 
 (set-unknown->c!
  (lambda (previous)
@@ -36,16 +37,38 @@
      (previous
       context
       (match operation
-        ['vdt-exp 'vdt_exp]
-        ;;['fast-sin 'sin]
-        ;;['fast-cos 'cos]
-        ;;['fast-tan 'tan]
-        ;;['fast-tanh 'tanh]
-        ;;['fast-log 'log]
-        ;;['fast-asin 'asin]
-        ;;['fast-acos 'acos]
-        ;;['fast-atan 'atan]
-        [operation operation])
+        ['fast-exp 'vdt::fast_exp]
+        ['fast-sin 'vdt::fast_sin]
+        ['fast-cos 'vdt::fast_cos]
+        ['fast-tan 'vdt::fast_tan]
+        ['fast-tanh 'vdt::fast_tanh]
+        ['fast-log 'vdt::fast_log]
+        ['fast-asin 'vdt::fast_asin]
+        ['fast-acos 'vdt::fast_acos]
+        ['fast-atan 'vdt::fast_atan]
+        ['fast-isqrt 'vdt::fast_isqrt]
+        [operation operation]
+        )
       arguments))))
 
-(define-accelerator-impl vdt-exp vdt-exp.f64 (binary64) binary64 (get-ffi-obj "vdt_exp" vdt-wrapper (_fun _double -> _double)))
+(define vdt-exp (get-ffi-obj "exp" vdt-wrapper (_fun _double -> _double)))
+(define vdt-sin (get-ffi-obj "sin" vdt-wrapper (_fun _double -> _double)))
+(define vdt-cos (get-ffi-obj "cos" vdt-wrapper (_fun _double -> _double)))
+(define vdt-tan (get-ffi-obj "tan" vdt-wrapper (_fun _double -> _double)))
+(define vdt-tanh (get-ffi-obj "tanh" vdt-wrapper (_fun _double -> _double)))
+(define vdt-log (get-ffi-obj "log" vdt-wrapper (_fun _double -> _double)))
+(define vdt-asin (get-ffi-obj "asin" vdt-wrapper (_fun _double -> _double)))
+(define vdt-acos (get-ffi-obj "acos" vdt-wrapper (_fun _double -> _double)))
+(define vdt-atan (get-ffi-obj "atan" vdt-wrapper (_fun _double -> _double)))
+(define vdt-isqrt (get-ffi-obj "isqrt" vdt-wrapper (_fun _double -> _double)))
+
+(define vdt-expf (get-ffi-obj "expf" vdt-wrapper (_fun _float -> _float)))
+(define vdt-sinf (get-ffi-obj "sinf" vdt-wrapper (_fun _float -> _float)))
+(define vdt-cosf (get-ffi-obj "cosf" vdt-wrapper (_fun _float -> _float)))
+(define vdt-tanf (get-ffi-obj "tanf" vdt-wrapper (_fun _float -> _float)))
+(define vdt-tanhf (get-ffi-obj "tanhf" vdt-wrapper (_fun _float -> _float)))
+(define vdt-logf (get-ffi-obj "logf" vdt-wrapper (_fun _float -> _float)))
+(define vdt-asinf (get-ffi-obj "asinf" vdt-wrapper (_fun _float -> _float)))
+(define vdt-acosf (get-ffi-obj "acosf" vdt-wrapper (_fun _float -> _float)))
+(define vdt-atanf (get-ffi-obj "atanf" vdt-wrapper (_fun _float -> _float)))
+(define vdt-isqrtf (get-ffi-obj "isqrtf" vdt-wrapper (_fun _float -> _float)))
--- vdtwrapper.cpp
+++ vdtwrapper.cpp
@@ -1,8 +1,65 @@
-#include <vdtMath.h>
+#include <vdt/vdtMath.h>
 
 extern "C"
 {
-  double vdt_exp(double x) {
+  double exp(double x) {
     return vdt::fast_exp(x);
+  }
+  double sin(double x) {
+    return vdt::fast_sin(x);
+  }
+  double cos(double x) {
+    return vdt::fast_cos(x);
+  }
+  double tan(double x) {
+    return vdt::fast_tan(x);
+  }
+  double tanh(double x) {
+    return vdt::fast_tanh(x);
+  }
+  double log(double x) {
+    return vdt::fast_log(x);
+  }
+  double asin(double x) {
+    return vdt::fast_asin(x);
+  }
+  double acos(double x) {
+    return vdt::fast_acos(x);
+  }
+  double atan(double x) {
+    return vdt::fast_atan(x);
+  }
+  double isqrt(double x) {
+    return vdt::fast_isqrt(x);
+  }
+  float expf(float x) {
+    return vdt::fast_expf(x);
+  }
+  float sinf(float x) {
+    return vdt::fast_sinf(x);
+  }
+  float cosf(float x) {
+    return vdt::fast_cosf(x);
+  }
+  float tanf(float x) {
+    return vdt::fast_tanf(x);
+  }
+  float tanhf(float x) {
+    return vdt::fast_tanhf(x);
+  }
+  float logf(float x) {
+    return vdt::fast_logf(x);
+  }
+  float asinf(float x) {
+    return vdt::fast_asinf(x);
+  }
+  float acosf(float x) {
+    return vdt::fast_acosf(x);
+  }
+  float atanf(float x) {
+    return vdt::fast_atanf(x);
+  }
+  float isqrtf(float x) {
+    return vdt::fast_isqrtf(x);
   }
 }
